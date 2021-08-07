814b3c5e545e185801c5d05aee9e9fa35d7a7504
vcel <haramiya21@gmail.com>
2021-08-08 01:22:48 +0900

    ~ ep.117

---
+++ .history/src/App_20210808012212.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <LikeHeader header-text="ヘッダーテキスト">
+      <template v-slot:title>
+        <h1>こんにちは</h1>
+      </template>
+      <template v-slot:number>
+        <div>
+          <h2>トータルのいいね数</h2>
+          <p>{{number}}</p>
+        </div>
+      </template>
+    </LikeHeader>
+    <LikeNumber :total-number="number" @my-click="number=$event"></LikeNumber>
+    <LikeNumber :total-number="number"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <!-- keep-aliveで動的コンポーネントの中身（テキストボックスなど）をキャッシュする -->
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import LikeHeader from "./components/LikeHeader.vue"
+import About from "./components/About.vue"
+import Home from "./components/Home.vue"
+
+export default {
+  data() {
+    return {
+      number: 14,
+      currentComponent: "Home"
+    };
+  },
+  components: {
+    LikeHeader,
+    About,
+    Home
+    // keyとvalueが一緒なので、短縮してLikeHeaderだけでも良い
+  }
+}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid blue;
+}
+</style>
