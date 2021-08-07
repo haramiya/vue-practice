bc8e1f4f0476cac7201a677117bd2bea29c88d5e
vcel <haramiya21@gmail.com>
2021-08-07 22:16:11 +0900

    Udemy ~ep.86

---
+++ src/components/LikeHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>いいね</h1>
+  </div>
+</template>
+
+<script>
+</script>
---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>いいね（{{ number }}）</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      number: 5
+    };
+  }
+}
+</script>
+
+<style>
+</style>
